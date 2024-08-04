74a688a049e7b94edde641bde649214c90360a7c
Heheayush <165564358+Ayushpachpute@users.noreply.github.com>
2024-08-05 01:12:42 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
+const sr = ScrollReveal({
+  distance: "40px",
+  duration: 2500,
+  reset: true,
+});
+sr.revel('.logo',{delay:200, origin: 'left'});
